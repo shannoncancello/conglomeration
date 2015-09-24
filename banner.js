dc8832b1d2b003c657caa73156a7b72cf7987b32
shannoncancello <shannon.cancello@gmail.com>
2015-09-24 18:00:33 -0400

    adding some bootstrap

--- banner.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    var images = new Array('photos/avengers.jpg', 'photos/doctor.jpg', 'photos/tmng.jpg', 'photos/trek.jpg');
-    var l = images.length;
-    var random_no = Math.floor(l*Math.random());
-    document.getElementById("banner").src = images[random_no];
-  </script>
